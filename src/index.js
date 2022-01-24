aa916116870b1c56ea9a44684be9ab7ca9ba882a
rainbow615 <228375557+rainbow615@users.noreply.github.com>
2022-01-24 00:29:34 -0800

    Set up the global style and theme color

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
+import './assets/scss/app.scss';
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
---
+++ src/theme/theme.js
@@ -0,0 +1,11 @@
+const ColorTheme = {
+  palette: {
+    transparent: 'transparent',
+    white: '#FFF',
+    black: '#000',
+    primary: '',
+    secondary: '',
+  },
+}
+
+export default ColorTheme
