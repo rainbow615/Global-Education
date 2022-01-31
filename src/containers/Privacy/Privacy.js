19269cc8741525318d34bd8f71aa402ba68e5ceb
john-mcp <john@mcprotocols.io>
2022-01-30 21:56:19 -0800

    Privacy Policy

--- src/containers/Privacy/Privacy.js
+++ src/containers/Privacy/Privacy.js
@@ -30,37 +30,45 @@ const Privacy = () => {
         <BackButton type="link" icon={<ArrowLeftOutlined />} size="large">
           <RouterLink to={'/home'}>Back Home</RouterLink>
         </BackButton>
-        <Title>What is Lorem Ipsum?</Title>
+        <Title>Privacy Policy</Title>
         <Description>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-          a galley of type and scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum.
+        This privacy policy ("policy") will help you understand how Mission Critical Protocols ("us", "we", "our") uses and protects the data you provide to us when you visit and use mcprotocols.io (“website”, "service"). 
+        
+        We reserve the right to change this policy at any given time, of which you will be promptly updated. If you want to make sure that you are up to date with the latest changes, we advise you to frequently visit this page.
         </Description>
-        <Title>Why do we use it?</Title>
+        <Title>What User Data We Collect</Title>
         <Description>
-          It is a long established fact that a reader will be distracted by the readable content of
-          a page when looking at its layout. The point of using Lorem Ipsum is that it has a
-          more-or-less normal distribution of letters, as opposed to using 'Content here, content
-          here', making it look like readable English. Many desktop publishing packages and web page
-          editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'
-          will uncover many web sites still in their infancy. Various versions have evolved over the
-          years, sometimes by accident, sometimes on purpose (injected humour and the like).
+          When you visit the website, we may collect the following data:
+
+          -Your IP address.
+          -Your contact information and email address.
+          -Other information such as interests and preferences.
+          -Data profile regarding your online behavior on our website.
         </Description>
-        <Title>Where does it come from?</Title>
+        <Title>Why We Collect Your Data</Title>
         <Description>
-          There are many variations of passages of Lorem Ipsum available, but the majority have
-          suffered alteration in some form, by injected humour, or randomised words which don't look
-          even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
-          sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum
-          generators on the Internet tend to repeat predefined chunks as necessary, making this the
-          first true generator on the Internet. It uses a dictionary of over 200 Latin words,
-          combined with a handful of model sentence structures, to generate Lorem Ipsum which looks
-          reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected
-          humour, or non-characteristic words etc.
+          We are collecting your data for several reasons:
+
+          -To better understand your needs.
+          -To improve our services and products.
+          -To customize our website according to your online behavior and personal preferences.
+        </Description>
+        <Title>Safeguarding and Securing the Data</Title>
+        <Description>
+          Mission Critical Protocols is committed to securing your data and keeping it confidential. Mission Critical Protocols has done all in its power to prevent data theft, unauthorized access, and disclosure by implementing the latest technologies and software, which help us safeguard all the information we collect online.
+        </Description>
+        <Title>Our Cookie Policy</Title>
+        <Description>
+        Once you agree to allow our website to use cookies, you also agree to use the data it collects regarding your online behavior (analyze web traffic, web pages you spend the most time on, and websites you visit).
+
+        The data we collect by using cookies is used to customize our website to your needs. After we use the data for statistical analysis, the data is completely removed
+        from our systems.
+
+        Please note that cookies don't allow us to gain control of your computer in any way. They are strictly used to monitor which pages you find useful and which you do not so that we can provide a better experience for you.
+
+        If you want to disable cookies, you can do it by accessing the settings of your internet browser. (Provide links for cookie settings for major internet browsers).
+
+        Mission Critical Protocols will not lease, sell or distribute your personal information to any third parties, unless we have your permission. We might do so if the law forces us.
         </Description>
         <BackButton type="link" icon={<ArrowLeftOutlined />} size="large">
           <RouterLink to={'/home'}>Back Home</RouterLink>
