52e663917ce868f72ec4a7f228d7db768b80b20b
rainbow615 <228375557+rainbow615@users.noreply.github.com>
2022-03-11 17:10:20 -0800

    Add the protocols service

---
+++ src/services/protocolService.js
@@ -0,0 +1,21 @@
+import api, { useAPI } from './api'
+
+export function useProtocols(orgId) {
+  return useAPI(orgId ? `/protocols/${orgId}` : null)
+}
+
+export function useProtocol(id) {
+  return useAPI(id ? `protocol/${id}` : null)
+}
+
+export function createProtocol(payload) {
+  return api.post(`/protocols`, payload)
+}
+
+export function updateProtocol(id, payload) {
+  return api.put(`/protocols/${id}`, payload)
+}
+
+export function deleteProtocol(id) {
+  return api.delete(`/protocols/${id}`)
+}
