f38c36572ca77d7c09312adc177aa5b0ad71084f
rainbow615 <228375557+rainbow615@users.noreply.github.com>
2022-03-20 12:35:35 -0700

    Create the custom tooltip component and refactor with that

---
+++ src/components/CustomTooltip/CustomTooltip.js
@@ -0,0 +1,21 @@
+import { Tooltip } from 'antd'
+
+import ColorTheme from '../../theme/theme'
+import { CopyLabel } from './styles'
+
+const CustomTooltip = (props) => {
+  const { label, title, onVisibleChange } = props
+
+  return (
+    <Tooltip
+      title={title}
+      color="white"
+      overlayInnerStyle={{ color: ColorTheme.palette.textColor, minWidth: 234, minHeight: 66 }}
+      onVisibleChange={onVisibleChange}
+    >
+      <CopyLabel color="red">{label}</CopyLabel>
+    </Tooltip>
+  )
+}
+
+export default CustomTooltip
--- src/components/Education/List/styles.js
+++ src/components/Education/List/styles.js
@@ -1,11 +1,5 @@
 import styled from 'styled-components'
-import { Button, Tag } from 'antd'
-
-export const CopyLabel = styled(Tag)`
-  text-transform: uppercase;
-  cursor: pointer;
-  border-width: 2px;
-`
+import { Button } from 'antd'
 
 export const ActionButton = styled(Button)`
   width: 65px;
