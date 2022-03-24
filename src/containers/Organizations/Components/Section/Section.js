ccd96e477d5ee0e7c42b016eedaf1c3661be2365
rainbow615 <228375557+rainbow615@users.noreply.github.com>
2022-03-23 23:56:08 -0700

    Update the component sections form

--- src/containers/Organizations/Components/Section/Section.js
+++ src/containers/Organizations/Components/Section/Section.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Form, Input, Select, Button, Space, notification } from 'antd'
+import Switch from 'react-switch'
 
 import CustomBreadcrumb from '../../../../components/CustomBreadcrumb/CustomBreadcrumb'
 import ComponentForm from '../../../../components/Components/Form'
+
+const { Option } = Select
+
+const Tags = []
 
 const ComponentSectionForm = (props) => {
   const { orgId, orgName } = props
@@ -26,11 +32,58 @@ const ComponentSectionForm = (props) => {
     },
   ]
 
+  const [isOrdered, setIsOrdered] = useState(false)
+
+  const onChangeOrder = (checked) => {
+    setIsOrdered(checked)
+  }
+
   return (
     <React.Fragment>
       <CustomBreadcrumb items={breadCrumb} />
       <ComponentForm>
-        <h1 style={{ fontSize: '2rem' }}>Section form</h1>
+        <Form.Item
+          label="Content"
+          name="name"
+          hasFeedback
+          rules={[{ required: true, message: 'Please input a section name' }]}
+        >
+          <Input placeholder="Enter a section name" size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Tags"
+          name="tags"
+          className="tags"
+          hasFeedback
+          rules={[{ required: true, message: 'Please select 1 or more tag.' }]}
+        >
+          <Select
+            mode="multiple"
+            size="large"
+            allowClear
+            showSearch
+            showArrow
+            optionLabelProp="label"
+            filterSort={(optionA, optionB) =>
+              optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())
+            }
+          >
+            {Tags.map((tag, index) => (
+              <Option key={index} value={tag.id} label={tag.name}>
+                {tag.name}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item label="Ordered?" name="name">
+          <Space>
+            <Switch onChange={onChangeOrder} checked={isOrdered} />
+            <i>
+              Selecting this will number to everything contained in this Section based on their
+              order.
+            </i>
+          </Space>
+        </Form.Item>
       </ComponentForm>
     </React.Fragment>
   )
