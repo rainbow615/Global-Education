039b818209210fa8d27570d60d10fcaab82411c8
Natanael de Kross <natanael@dekross.com>
2022-04-13 09:30:12 -0400

    Add icons to the sidebar

--- src/containers/PortalLayout/PortalSidebar.js
+++ src/containers/PortalLayout/PortalSidebar.js
@@ -3,6 +3,7 @@ import { Menu } from 'antd'
 
 import { getMainPathname } from '../../utils'
 import { CustomSide } from './styles'
+import { FileProtectOutlined, FundOutlined, HomeOutlined, TeamOutlined } from '@ant-design/icons'
 
 const navLinks = [
   {
@@ -27,13 +28,20 @@ const PortalSidebar = () => {
   const location = useLocation()
   const mainPath = getMainPathname(location.pathname)
 
+  const icons = {
+    Organizations: <HomeOutlined />,
+    'Global Education': <FundOutlined />,
+    Users: <TeamOutlined />,
+    Licenses: <FileProtectOutlined />,
+  }
+
   return (
-    <CustomSide width={200} theme="light">
+    <CustomSide width={200} theme="light" collapsible>
       <Menu theme="light" selectedKeys={[mainPath]}>
         {navLinks.map((navLink) => {
           const { path, pathName } = navLink
           return (
-            <Menu.Item key={path}>
+            <Menu.Item key={path} icon={icons[pathName]}>
               <NavLink to={path}>{pathName}</NavLink>
             </Menu.Item>
           )
