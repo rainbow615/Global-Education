63056726d91591d806a06dc112223533fb969328
Natanael de Kross <natanael@dekross.com>
2022-04-13 12:15:16 -0400

    Display data on Dashboard view

--- src/containers/Organizations/Dashboard/Dashboard.js
+++ src/containers/Organizations/Dashboard/Dashboard.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import CustomBreadcrumb from '../../../components/CustomBreadcrumb/CustomBreadcrumb'
 import { Container } from '../../../components/CommonComponent'
-import { DashboardContainer } from './styles'
-import { Link, useLocation } from 'react-router-dom'
-import { Card } from 'antd'
+import { CardBottom, ComponentType, CustomCard, CustomTable, DashboardContainer } from './styles'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Button, Card, Dropdown, Typography } from 'antd'
 import { ArrowsAltOutlined } from '@ant-design/icons'
+import { useProtocols } from '../../../services/protocolService'
+import { ResultFailed } from '../../../components/ResultPages'
+import {
+  getJITStatusName,
+  getProtocolStatusColor,
+  getProtocolStatusName,
+} from '../../../utils/names'
+import CopyTooltip from '../Protocols/List/CopyTooltip'
+import { useEducations } from '../../../services/jitService'
+import { useComponents } from '../../../services/componentService'
+import AddComponentButton from '../../../components/Components/AddComponentButton'
+import { returnLinks } from '../Protocols/List/columns'
+import { getEducationReturnLinks } from '../../../components/Education/List/columns'
 
 const Dashboard = () => {
   const { state } = useLocation()
+
+  const navigate = useNavigate()
+
+  const { data: protocols, error: protocolError } = useProtocols(state.id)
+
+  const { data: education, error: educationError } = useEducations(state.id)
+
+  const { data: components, error: componentError } = useComponents(state.id)
+
+  const error = protocolError || educationError || componentError
+
+  const protocolsDataSource = protocols?.data?.slice(0, 9)
+  const educationDataSource = education?.data?.slice(0, 9)
+  const componentsDataSource = components?.data?.slice(0, 9)
+
+  if (error) {
+    return <ResultFailed isBackButton={false} />
+  }
 
   const breadCrumb = [
     {
@@ -31,22 +62,120 @@ const Dashboard = () => {
       <CustomBreadcrumb items={breadCrumb} />
       <Container>
         <DashboardContainer>
-          <Card
+          <CustomCard
             title="Protocols"
             extra={
               <Link {...generateLinkProps('/organizations/protocols/list')}>
                 <ArrowsAltOutlined />
               </Link>
             }
-          ></Card>
-          <Card
+          >
+            <CustomTable
+              pagination={false}
+              showHeader={false}
+              scroll={true}
+              loading={protocols.isLoading}
+              dataSource={protocolsDataSource}
+              onRow={(record) => ({
+                onClick: () =>
+                  navigate(returnLinks[record.status], {
+                    state: {
+                      orgId: state.id,
+                      orgName: state.name,
+                    },
+                  }),
+              })}
+              columns={[
+                {
+                  title: 'Number',
+                  dataIndex: 'protocol_number',
+                  key: 'protocol_number',
+                  width: 100,
+                },
+                {
+                  title: 'Name',
+                  dataIndex: 'protocol_name',
+                  key: 'protocol_name',
+                  render: (value, record) => <CopyTooltip value={value} record={record} />,
+                },
+                {
+                  title: 'Status',
+                  dataIndex: 'status',
+                  key: 'status',
+                  width: 100,
+                  render: (value) => (
+                    <Typography.Text type={getProtocolStatusColor(value)}>
+                      {getProtocolStatusName(value)}
+                    </Typography.Text>
+                  ),
+                },
+              ]}
+            />
+            <CardBottom>
+              <Button type="primary">
+                <Link
+                  to="/organizations/protocols/form/new"
+                  state={{ orgId: state.id, orgName: state.name }}
+                >
+                  Add new
+                </Link>
+              </Button>
+            </CardBottom>
+          </CustomCard>
+          <CustomCard
             title="Education"
             extra={
               <Link {...generateLinkProps('/organizations/local-education/list')}>
                 <ArrowsAltOutlined />
               </Link>
             }
-          ></Card>
+          >
+            <CustomTable
+              pagination={false}
+              showHeader={false}
+              scroll={true}
+              onRow={(record) => ({
+                onClick: () =>
+                  navigate(getEducationReturnLinks(false)[record.status], {
+                    state: {
+                      orgId: state.id,
+                      orgName: state.name,
+                    },
+                  }),
+              })}
+              loading={education.isLoading}
+              dataSource={educationDataSource}
+              columns={[
+                {
+                  title: 'Name',
+                  dataIndex: 'jit_name',
+                  key: 'jit_name',
+                  render: (value, record) => <CopyTooltip value={value} record={record} />,
+                },
+                {
+                  title: 'Status',
+                  dataIndex: 'status',
+                  key: 'status',
+                  width: 100,
+                  render: (value) => (
+                    <Typography.Text type={getProtocolStatusColor(value)}>
+                      {getJITStatusName(value)}
+                    </Typography.Text>
+                  ),
+                },
+              ]}
+            />
+            <CardBottom>
+              <Button type="primary">
+                <Link
+                  to="/organizations/local-education/form/new"
+                  state={{ orgId: state.id, orgName: state.name }}
+                >
+                  Add new
+                </Link>
+              </Button>
+            </CardBottom>
+          </CustomCard>
           <Card
             title="Components"
             extra={
@@ -54,7 +183,53 @@ const Dashboard = () => {
                 <ArrowsAltOutlined />
               </Link>
             }
-          ></Card>
+          >
+            <CustomTable
+              pagination={false}
+              scroll={true}
+              showHeader={false}
+              loading={components.isLoading}
+              onRow={(record) => ({
+                onClick: () => {
+                  console.log(record)
+                  navigate(`/organizations/components/form/${record.component_type}/edit`, {
+                    state: { ...record, orgId: state.id, orgName: state.name },
+                  })
+                },
+              })}
+              dataSource={componentsDataSource}
+              columns={[
+                {
+                  title: 'Type',
+                  dataIndex: 'component_type',
+                  key: 'component_type',
+                  width: 10,
+                  render: (value) => <ComponentType>{value[0].toUpperCase()}</ComponentType>,
+                },
+                {
+                  title: 'Content',
+                  dataIndex: 'component_content',
+                  key: 'component_content',
+                  render: (value) => <div dangerouslySetInnerHTML={{ __html: value }} />,
+                },
+                {
+                  title: 'Usage Amount',
+                  dataIndex: 'backref',
+                  key: 'backref',
+                  render: (value) => <Typography.Text>{value ? value.length : 0}</Typography.Text>,
+                },
+                {
+                  title: 'Linked Education',
+                  dataIndex: 'linked_education',
+                  key: 'linked_education',
+                  render: (value) => <Typography.Text>{value ? value.length : 0}</Typography.Text>,
+                },
+              ]}
+            />
+            <CardBottom>
+              <AddComponentButton orgId={state.id} orgName={state.name} />
+            </CardBottom>
+          </Card>
           <Card title="Activity"></Card>
         </DashboardContainer>
       </Container>
--- src/containers/Organizations/Dashboard/styles.js
+++ src/containers/Organizations/Dashboard/styles.js
@@ -1,8 +1,46 @@
+import { Card, Table } from 'antd'
 import styled from 'styled-components'
 
 export const DashboardContainer = styled.div({
   width: '100%',
   display: 'grid',
   gap: '1rem',
-  gridTemplateColumns: 'repeat(auto-fit, minmax(min(350px, 100%), 1fr))',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(min(400px, 100%), 1fr))',
 })
+
+export const CustomTable = styled(Table)({
+  height: '15rem',
+  overflowY: 'scroll',
+  tr: {
+    cursor: 'pointer',
+  },
+  'tr:last-of-type td': {
+    borderBottom: 'none',
+  },
+  'td: first-of-type': {
+    paddingLeft: 0,
+  },
+})
+
+export const CustomCard = styled(Card)({
+  padding: '0.25rem 0.125rem .5rem',
+  '.ant-card-body': {
+    paddingTop: 1,
+  },
+})
+
+export const CardBottom = styled.div({
+  display: 'flex',
+  justifyContent: 'flex-end',
+  marginTop: '1rem',
+})
+
+export const ComponentType = styled.span(({ theme }) => ({
+  background: theme.palette.blueGrey[50],
+  width: '1.5rem',
+  height: '1.5rem',
+  display: 'inline-flex',
+  alignContent: 'center',
+  justifyContent: 'center',
+  border: '1px solid ' + theme.palette.blueGrey[100],
+}))
