dbbacee10f8fdfc63c5f258c4f14a90ea82c6b7f
Natanael de Kross <natanael@dekross.com>
2022-04-22 08:20:26 -0400

    Improve user role tag styling

--- src/containers/Users/List/styles.js
+++ src/containers/Users/List/styles.js
@@ -1,1 +1,9 @@
+import { Tag } from 'antd'
 import styled from 'styled-components'
+
+export const CustomTag = styled(Tag)({
+  fontSize: '0.65rem',
+  fontFamily: 'InterMedium',
+  letterSpacing: 0.5,
+  color: 'grey',
+})
